d43485366528cfba4b40adb9caf4a00c31aed68c
Kevin Sieracke <ksieracke@gmail.com>
2024-01-27 15:36:06 -0600

    fetch gallery set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,30 @@
+import axios from 'axios';
+import React from 'react';
+import {useState, useEffect} from 'react'
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 function App() {
+
+    const [gallery, setGallery]=useState([]);
+
+    const fetchGallery =()=>{
+      axios.get('/api/gallery')
+        .then((response)=>{
+          console.log(response.data);
+          setGallery(response.data);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+    }
+
+    useEffect(()=>{
+      fetchGallery();
+    }, [])
+
     return (
-      <div>
+      <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
         </header>
