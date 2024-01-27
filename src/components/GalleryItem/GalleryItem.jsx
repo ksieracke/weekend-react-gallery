acd3d0b7fbbadbb81cf2f2b72f6da78de19b2f4c
Kevin Sieracke <ksieracke@gmail.com>
2024-01-27 16:12:23 -0600

    like button added, still need to link to DB

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,11 +2,19 @@
 import React from "react";
 import { useState } from "react";
 
+
 function GalleryItem({pic}){
      const [showPic, setShowPic]=useState(true);
+     const [likes, setLikes]=useState(pic.likes)
 
      const togglePicAndDescription=()=>{
         setShowPic(!showPic);
+     }
+
+     const addLike=(event)=>{
+        console.log('like button clicked');
+        setLikes(likes +1);
+
      }
 
      return(
@@ -16,6 +24,8 @@ function GalleryItem({pic}){
                 <img src={pic.url} /> :<p>{pic.description}</p>
                }
             </div>
+            <p>Likes: {likes}  <button onClick={addLike}>Like</button></p>
+
         </div>
      )
 
